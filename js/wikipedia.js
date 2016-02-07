cdaa9e5326bb5fece03160d9131468b1d6a80ac2
The-Penultimate-Defenestrator <ldt@deentaylor.com>
2016-02-07 11:19:15 -0500

    Add a new input method
    Add an input box. Type in this box to choose a page. Press enter to submit. If you change the contents of this box and press enter again, the network will be reset

--- js/wikipedia.js
+++ js/wikipedia.js
@@ -1,50 +1,68 @@
-var startpage = getPageName(prompt("Name a wikipedia page:"));
-startpage = wordwrap(startpage,20)
+var nodes, edges, startpage //Global variables
+
+var inputBox = document.getElementById('pageName');
+
+//Reset the network with the page searched when enter pressed inside the input
+inputBox.onkeypress = function(e) {
+    var event = e || window.event;
+    var charCode = event.which || event.keyCode;
+
+    if ( charCode == '13' ) {
 
 
-// -- CREATE NETWORK -- //
-//Make a container
-var nodes = new vis.DataSet([{id:startpage,label:startpage,
-                              value:2,level:0,color:getColor(0),
-                              parent:startpage}]); //Parent is self
-var edges = new vis.DataSet();
-//Put the data in the container
-var data = {nodes:nodes,edges:edges};
-var container = document.getElementById('container');
-//Global options
-var options = {
-  nodes: {
-    shape: 'dot',
-    scaling: { min: 20,max: 30,
-      label: { min: 14, max: 30, drawThreshold: 9, maxVisible: 20 }
-    },
-    font: {size: 12, face: 'Arial'}
-  },
-};
-//Make the network
-var network = new vis.Network(container,data,options);
+
+      startpage = getPageName(document.getElementById('pageName').value);
+      startpage = wordwrap(startpage,20)
 
 
-// -- ACTIONS -- //
+      // -- CREATE NETWORK -- //
+      //Make a container
+      nodes = new vis.DataSet([{id:startpage,label:startpage,
+                                    value:2,level:0,color:getColor(0),
+                                    parent:startpage}]); //Parent is self
+      edges = new vis.DataSet();
+      //Put the data in the container
+      var data = {nodes:nodes,edges:edges};
+      var container = document.getElementById('container');
+      //Global options
+      var options = {
+        nodes: {
+          shape: 'dot',
+          scaling: { min: 20,max: 30,
+            label: { min: 14, max: 30, drawThreshold: 9, maxVisible: 20 }
+          },
+          font: {size: 12, face: 'Arial'}
+        },
+      };
+      //Make the network
+      var network = new vis.Network(container,data,options);
 
-//Open a node when clicked
-network.on("doubleClick", function (params) {
-  if (params.nodes.length) { //Did the click occur on a node?
-    var page = params.nodes[0]; //The node clicked
-    expandNode(page);
-  }
-});
 
-//Highlight traceback on click
-network.on("click", function (params) {
-  if (params.nodes.length) { //Was the click on a node?
-    //The node clicked
-    var page = params.nodes[0];
-    //Re-orange all nodes
-    resetProperties();
-    //Highlight in blue all nodes tracing back to central node
-    traceBack(page);
-  } else {
-    resetProperties();
-  }
-});
+      // -- ACTIONS -- //
+
+      //Open a node when clicked
+      network.on("doubleClick", function (params) {
+        if (params.nodes.length) { //Did the click occur on a node?
+          var page = params.nodes[0]; //The node clicked
+          expandNode(page);
+        }
+      });
+
+      //Highlight traceback on click
+      network.on("click", function (params) {
+        if (params.nodes.length) { //Was the click on a node?
+          //The node clicked
+          var page = params.nodes[0];
+          //Re-orange all nodes
+          resetProperties();
+          //Highlight in blue all nodes tracing back to central node
+          traceBack(page);
+        } else {
+          resetProperties();
+        }
+      });
+
+
+
+    }
+}
