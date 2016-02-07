6d5b0824dda2087b7d0f8e6d53d5ec5941178944
The-Penultimate-Defenestrator <ldt@deentaylor.com>
2016-02-07 17:55:55 -0500

    Add a button to submit input
    Now there's an alternate method to submit your input, besides just pressing enter. Also in this update, if no page is provied, the app will default to Wikipedia

--- js/bindings.js
+++ js/bindings.js
@@ -1,15 +1,22 @@
 var inputBox = document.getElementById('pageName');
 
-//Reset the network with the page searched when enter pressed inside the input
+//Bind the event of pressing enter inside the text box
 inputBox.onkeypress = function(e) {
-
   var event = e || window.event;
   var charCode = event.which || event.keyCode;
-
   if ( charCode == '13' ) { //Enter was pressed inside dialog
-
-    startpage = getPageName(document.getElementById('pageName').value);
+    var pagename = document.getElementById('pageName').value || "Wikipedia";
+    startpage = getPageName(pagename);
     createNetwork(startpage);
-
   }
+}
+
+
+//Bind the action of pressing the button
+var submitButton = document.getElementById('submit');
+
+submitButton.onclick = function() {
+  var pagename = document.getElementById('pageName').value || "Wikipedia";
+  startpage = getPageName(pagename);
+  createNetwork(startpage);
 }
