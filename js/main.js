7a5d54bde2f39b01381ea1490d279fae90c47938
The-Penultimate-Defenestrator <ldt@deentaylor.com>
2016-02-16 19:05:55 -0500

    Implement style and restore README
    Now most of the style for the menu is in place. It is intentionally styled to look similar to the iOS UINavigationController, as is seen in the iOS "Settings" app. Also restore the README to the menu so that it is useful again. If you open the "help" menu and press "README", you will see the README along with a floating "back" button. The "About" and "Controls" pages have a heading that says "Not yet implemented" and a button that goes back to the main menu page.

--- js/main.js
+++ js/main.js
@@ -14,11 +14,11 @@ var options = {
     },
     font: {size: 12, face: 'Arial'}
   },
-  interaction:{
+  interaction: {
     hover: true,
     hoverConnectedEdges: false,
     selectConnectedEdges: true,
-  }
+  },
 };
 
 var nodes = new vis.DataSet();
@@ -43,9 +43,10 @@ function resetNetwork(start) {
   traceedges = [];
   // -- CREATE NETWORK -- //
   //Make a container
-  nodes = new vis.DataSet([{id:startpage,label:wordwrap(startpage,20),
-                                value:2,level:0,color:getColor(0),
-                                parent:startpage}]); //Parent is self
+  nodes = new vis.DataSet([
+    {id:startpage, label:wordwrap(startpage,20), value:2, level:0,
+     color:getColor(0), parent:startpage} //Parent is self
+  ]);
   edges = new vis.DataSet();
   //Put the data in the container
   data = {nodes:nodes,edges:edges};
