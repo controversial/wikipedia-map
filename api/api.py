da9d0bbe62aaa56017b6e2dd8e7dd9b5685ddca4
Luke Taylor <ldt@deentaylor.com>
2016-03-05 17:46:03 -0500

    Complete API rewrite \o/ 🎉
    Completely rewrote the API backend. Before, it parsed through the page HTML for entire Wikipedia articles. This was slow and inefficient, but more importantly it violated the Wikimedia bot policy (https://meta.wikimedia.org/wiki/Bot_policy#Unacceptable_usage). Now, this project uses the official WikiMedia API. This has countless advantages:
    
    1. It's legal under WikiMedia's bot policy
    2. It's faster. Things like fetching page title no longer need to involve loading the entire page HTML into a BeautifulSoup object. Now it's a fast API call. Instead of parsing thousands of characters of HTML, it needs to parse under 100 characters of JSON. Also on speed, now the HTML loading that *does* happen is much less, since the MediaWiki API lets me only get the HTML of the first section. This makes other stuff faster too.
    3. It opens up new possibilities. Faster calls to get_page_name could (finally) lead to a realistic (speed-wise) solution to issue #2.
    
    I'm excited about this. It's probably not actually that exciting. But oh well :)

--- api/api.py
+++ api/api.py
@@ -1,27 +1,31 @@
 # coding: utf-8
 
 from __future__ import unicode_literals
-from flask import Flask,request
 import json
 
-#My wikipedia API
+from flask import Flask, request
+
+# My wikipedia API
 from wikipedia_parse import *
 
 app = Flask(__name__)
 
+
 @app.route('/links')
 def getSubPages():
-    page=request.args.get("page")
+    page = request.args.get("page")
     return json.dumps(first_paragraph_links(page))
+
 
 @app.route('/pagename')
 def getPageName():
-    page=request.args.get("page")
+    page = request.args.get("page")
     return json.dumps(get_page_name(page))
+
 
 @app.route('/random')
 def randomArticle():
-  return get_random_article()
+    return get_random_article()
 
 if __name__ == "__main__":
     app.run()
--- api/api.wsgi
+++ api/api.wsgi
@@ -1,3 +1,4 @@
 import sys
-sys.path.insert(0,"/var/www/deentaylor.com/luke/public_html/wikipedia/api")
+sys.path.insert(0, "/var/www/deentaylor.com/luke/public_html/wikipedia/api")
+
 from api import app as application
