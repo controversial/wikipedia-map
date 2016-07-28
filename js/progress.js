912777d5d5257486f0f0d9c7b5a4d2983f23c566
The-Penultimate-Defenestrator <ldt@deentaylor.com>
2016-07-27 23:14:04 -0400

    Add label to progress bar, take out transition
    Transition caused issues in network because of many blocking DOM updates

--- js/progress.js
+++ js/progress.js
@@ -14,17 +14,28 @@
 function Progress(mainclass, barclass) {
   mainclass = mainclass || "";
   barclass = barclass || "";
+  this.container = document.createElement("div");
+  // Create the progress bar
   this.elem = document.createElement("div");
   this.elem.className = mainclass + " progressbar";
   this.bar = document.createElement("div");
   this.bar.className = barclass + " progressbar-indicator";
+  // Create the label
+  this.label = document.createElement("div");
+  this.label.className = "progressbar-label";
+  this.label.textContent = "0";
+
   this.elem.appendChild(this.bar);
+  this.container.appendChild(this.elem);
+  this.container.appendChild(this.label);
+
   // Start at 0%
   this.bar.style.width = "0px";
   // Function to set progress
   this.progress = function(amount) {
     if (amount !== undefined) {
-      this.bar.style.width = this.elem.offsetWidth * amount + "px";
+      this.bar.style.width = amount * 100 + "%";
+      this.label.textContent = Math.floor(amount * 100);
     } else {
       return this.bar.offsetWidth / this.elem.offsetWidth;
     }
