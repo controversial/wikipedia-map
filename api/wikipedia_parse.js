c2455aca5ef705d72ebb379b0977eab92524cfbe
Luke Taylor <luke@deentaylor.com>
2017-06-26 23:33:15 -0700

    Add function for getPageName

--- api/wikipedia_parse.js
+++ api/wikipedia_parse.js
@@ -2,6 +2,8 @@ const request = require('superagent');
 const { URL } = require('url');
 const path = require('path');
 
+const endpoint = 'https://en.wikipedia.org/w/api.php';
+
 /**
 Get the title of a page from a URL quickly,but inaccurately. Allows both for URLs with a trailing
 slash and for URLs without.
@@ -12,4 +14,24 @@ This is considered inaccurate because this function not handle redirects, e.g. /
 exports.getPageTitle = function getPageTitle(url) {
   const p = new URL(url).pathname;
   return path.basename(p);
+}
+
+/**
+Get the name of a Wikipedia page accurately by following redirects (slow)
+*/
+exports.getPageName = function getPageName(page) {
+  return new Promise((resolve, reject) => {
+    request
+      .get(endpoint)
+      .query({
+        format: 'json',
+        action: 'query',
+        titles: page,
+        redirects: 1,
+      })
+      .end((err, res) => {
+        if (err) reject(err);
+        else resolve(Object.values(res.body.query.pages)[0].title)
+      })
+  });
 }
