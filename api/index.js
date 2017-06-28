d604c0721c904db341e72c3992ff9fa3685a4342
Luke Taylor <luke@deentaylor.com>
2017-06-27 22:18:34 -0700

    req.query, not res.query

--- api/index.js
+++ api/index.js
@@ -1,21 +1,24 @@
 const express = require('express');
 
+const wp = require('./wikipedia_parse');
+
+
 const app = express();
 app.set('json spaces', 2);
 
 
 app.get('/links', (req, res) => {
-  const page = res.query.page;
+  const page = req.query.page;
 });
 
 app.get('/pagename', (req, res) => {
-  const page = res.query.page;
+  const page = req.query.page;
 });
 
 app.get('/random', (req, res) => {});
 
 app.get('/suggest', (req, res) => {
-  const text = res.query.text;
+  const text = req.query.text;
 });
 
 app.get('/storejson', (req, res) => {
