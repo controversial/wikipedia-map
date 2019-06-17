8a20608de1d28afb0cd01cfa4142a0eacc95370d
Luke Taylor <luke@deentaylor.com>
2019-06-16 22:33:07 -0400

    Update bindings.js style

--- js/bindings.js
+++ js/bindings.js
@@ -1,85 +1,73 @@
+/* global nodes, network, isTouchDevice, shepherd */
+/* global expandNode, traceBack, resetProperties, resetNetworkFromInput, randomReset, unwrap */
 // This script contains (most of) the code that binds actions to events.
 
 
-//Functions that will be used as bindings
-function expandEvent (params) { // Expand a node (with event handler)
-  if (params.nodes.length) { //Did the click occur on a node?
-    var page = params.nodes[0]; //The id of the node clicked
+// Functions that will be used as bindings
+function expandEvent(params) { // Expand a node (with event handler)
+  if (params.nodes.length) { // Did the click occur on a node?
+    const page = params.nodes[0]; // The id of the node clicked
     expandNode(page);
   }
 }
 
-function mobileTraceEvent (params) { // Trace back a node (with event handler)
-  if (params.nodes.length) { //Was the click on a node?
-    //The node clicked
-    var page = params.nodes[0];
-    //Highlight in blue all nodes tracing back to central node
+function mobileTraceEvent(params) { // Trace back a node (with event handler)
+  if (params.nodes.length) { // Was the click on a node?
+    // The node clicked
+    const page = params.nodes[0];
+    // Highlight in blue all nodes tracing back to central node
     traceBack(page);
   } else {
     resetProperties();
   }
 }
 
-function openPageEvent (params) {
+function openPageEvent(params) {
   if (params.nodes.length) {
-    var nodeid = params.nodes[0];
-    var page = encodeURIComponent(unwrap(nodes.get(nodeid).label));
-    var url = "http://en.wikipedia.org/wiki/"+page;
+    const nodeid = params.nodes[0];
+    const page = encodeURIComponent(unwrap(nodes.get(nodeid).label));
+    const url = `http://en.wikipedia.org/wiki/${page}`;
     window.open(url, '_blank');
   }
 }
 
 // Bind the network events
-function bindNetwork(){
+function bindNetwork() {
   if (isTouchDevice) { // Device has touchscreen
-    network.on("hold", expandEvent); // Long press to expand
-    network.on("click", mobileTraceEvent); // Highlight traceback on click
+    network.on('hold', expandEvent); // Long press to expand
+    network.on('click', mobileTraceEvent); // Highlight traceback on click
   } else { // Device does not have touchscreen
-    network.on("click", expandEvent); // Expand on click
-    network.on("hoverNode", function(params){ // Highlight traceback on hover
-      traceBack(params.node);
-    });
-    network.on("blurNode", resetProperties); // un-traceback on un-hover
+    network.on('click', expandEvent); // Expand on click
+    network.on('hoverNode', params => traceBack(params.node)); // Highlight traceback on hover
+    network.on('blurNode', resetProperties); // un-traceback on un-hover
   }
 
-  //Bind double-click to open page
-  network.on("doubleClick", openPageEvent);
+  // Bind double-click to open page
+  network.on('doubleClick', openPageEvent);
 }
 
 function bind() {
-
   // Prevent iOS scrolling
-  document.ontouchmove = function(event){
-    event.preventDefault();
-  };
+  document.addEventListener('touchmove', e => e.preventDefault());
 
   // Bind actions for search component.
 
-  var cf = document.getElementsByClassName("commafield")[0];
-  //Bind the action of pressing the button
-  var submitButton = document.getElementById('submit');
-  submitButton.onclick = function() {
+  const cf = document.querySelector('.commafield');
+  // Bind go button press
+  const submitButton = document.getElementById('submit');
+  submitButton.addEventListener('click', () => {
     shepherd.cancel(); // Dismiss the tour if it is in progress
     resetNetworkFromInput();
-  };
+  });
 
-  var randomButton = document.getElementById('random');
-  randomButton.onclick = randomReset;
+  const randomButton = document.getElementById('random');
+  randomButton.addEventListener('click', randomReset);
 
   // Bind tour start
-  var tourbtn = document.getElementById("tourinit");
-  tourbtn.onclick = function(){shepherd.start();};
+  const tourbtn = document.getElementById('tourinit');
+  tourbtn.addEventListener('click', () => shepherd.start());
 
   // Bind GitHub button
-  var ghbutton = document.getElementById("github");
-  ghbutton.onclick = function(event) {
-    window.open("https://github.com/The-Penultimate-Defenestrator/wikipedia-map");
-  };
-
-  // Bind twitter button
-  var sharebutton = document.getElementById("share");
-  var buttons = document.getElementById("buttons");
-  sharebutton.onclick = function(event) {
-
-  };
+  const ghbutton = document.getElementById('github');
+  ghbutton.addEventListener('click', () => window.open('https://github.com/controversial/wikipedia-map', '_blank'));
 }
