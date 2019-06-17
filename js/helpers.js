0b9a81bcce85ffb0ac183272d498a0d76920b138
Luke Taylor <luke@deentaylor.com>
2019-06-16 22:33:24 -0400

    Update helpers.js style

--- js/helpers.js
+++ js/helpers.js
@@ -1,3 +1,4 @@
+/* global vis, network, nodes, edges, isReset:writable */
 // This script contains helper functions that are used by other scripts to
 // perform simple common actions.
 
@@ -6,102 +7,87 @@
 
 // Get the level of the highest level node that exists in the graph
 function maxLevel() {
-  var ids = nodes.getIds();
-  levels = ids.map(function(x){return nodes.get(x).level;});
+  const ids = nodes.getIds();
+  const levels = ids.map(x => nodes.get(x).level);
   return Math.max.apply(null, levels);
 }
 
 // Convert a hex value to RGB
 function hexToRGB(hex) {
-  if (hex[0] == "#"){hex = hex.slice(1,hex.length);} // Remove leading #
-  strips=[hex.slice(0,2),hex.slice(2,4),hex.slice(4,6)]; // Cut up into 2-digit strips
-  return strips.map(function(x){return parseInt(x,16);}); // To RGB
+  // eslint-disable-next-line no-param-reassign
+  if (hex.startsWith('#')) hex = hex.slice(1, hex.length); // Remove leading #
+  const strips = [hex.slice(0, 2), hex.slice(2, 4), hex.slice(4, 6)]; // Cut up into 2-digit strips
+  return strips.map(x => parseInt(x, 16)); // To RGB
 }
 function rgbToHex(rgb) {
-  var hexvals = rgb.map(function(x){return Math.round(x).toString(16);});
+  const hexvals = rgb
+    .map(x => Math.round(x).toString(16))
+    .map(x => (x.length === 1 ? `0${x}` : x));
   // Add leading 0s to make a valid 6 digit hex
-  hexvals = hexvals.map(function(x){
-    return x.length == 1 ? "0"+x : x;
-  });
-  return "#"+hexvals.join("");
+  return `#${hexvals.join('')}`;
 }
+
 // Lighten a given hex color by %
-function lightenHex(hex,percent) {
-  var rgb = hexToRGB(hex); // Convert to RGB
-  if (percent>100) percent=100; //Limit to 100%
-  var newRgb = rgb.map(function(x){
-    return x+percent/100.0*(255-x); // This works because math.
-  });
-  return rgbToHex(newRgb); //and back to hex
+function lightenHex(hex, percent) {
+  const rgb = hexToRGB(hex); // Convert to RGB
+  const newRgb = rgb.map(x => x + ((Math.min(percent, 100) / 100) * (255 - x)));
+  return rgbToHex(newRgb); // and back to hex
 }
 // Get the color for a node, lighten a blue based on level. Subtle.
 function getColor(level) {
-  return lightenHex("#03A9F4",5*level); // Gets 5% lighter for each level
+  return lightenHex('#03A9F4', 5 * level); // Gets 5% lighter for each level
 }
 // Get the highlighted color for a node, lighten a yellow based on level. Subtle.
 function getYellowColor(level) {
-  return lightenHex("#FFC107",5*level); // Gets 5% lighter for each level
+  return lightenHex('#FFC107', 5 * level); // Gets 5% lighter for each level
 }
 // Get the color that an edge should be pointing to a certain level
 function getEdgeColor(level) {
-  var nodecolor = getColor(level);
+  const nodecolor = getColor(level);
   return vis.util.parseColor(nodecolor).border;
 }
 
 
 // Break a sentence into separate lines, trying to fit each line within `limit`
 // characters. Only break at spaces, never break in the middle of words.
-function wordwrap(text,limit) {
-  var words = text.split(" ");
-  var lines = [""];
-  for (var i=0; i<words.length; i++) {
-    var word = words[i];
-    lastLine = lines[lines.length-1];
-    if (lastLine.length + word.length > limit) {
-      lines.push(word);
-    } else {
-      lines[lines.length-1] = lastLine + " " + word;
-    }
-  }
-  return lines.join("\n").trim(); // Trim because the first line will start with a space
+function wordwrap(text, limit) {
+  const words = text.split(' ');
+  const lines = [words[0]];
+  words.slice(1).forEach((word) => {
+    // Start a new line if adding this word to the previous line would overflow character limit
+    if (lines[lines.length - 1].length + word.length > limit) lines.push(word);
+    else lines[lines.length - 1] += ` ${word}`;
+  });
+  return lines.join('\n'); // Trim because the first line will start with a space
 }
 // Un-word wrap a sentence by replacing line breaks with spaces.
-function unwrap(text) {
-  return text.replace(/\n/g," ");
-}
+function unwrap(text) { return text.replace(/\n/g, ' '); }
 
 // Get a "neutral" form of a page name to use as an ID. This is designed to
 // minimize the number of duplicate nodes found in the network.
 function getNeutralId(id) {
-  id = id.toLowerCase(); // Lowercase
-  id = id.replace( /%20/g , "" ); // Remove code for spaces
-  id = id.replace(/[^A-Za-z\d%]/g, ""); // Remove all non-alphanumeric characters
-  if (id[id.length-1] == "s") {
-    id = id.slice(0, -1);
-  }
-  return id;
+  return id
+    .toLowerCase() // Lowercase
+    .replace(/\s/g, '') // Remove spaces
+    .replace(/[^A-Za-z\d%]/g, '') // Remove non-alphanumeric characters
+    .replace(/s$/, ''); // Remove trailing s
 }
 
 // A cross-browser compatible alternative to Math.sign, because support is atrocious
 function sign(x) {
-  if (Math.sign) {
-    return Math.sign(x);
-  } else if (x === 0) {
-    return 0;
-  } else {
-    return x > 0 ? 1:-1;
-  }
+  if (x === 0) return 0;
+  return x > 0 ? 1 : -1;
 }
 
 
 // == NETWORK SHORTCUTS == //
 
 // Color nodes from a list based on their level. If color=1, highlight color will be used.
-function colorNodes(ns,color) {
-  var colorFunc = color ? getYellowColor : getColor;
+function colorNodes(ns, color) {
+  const colorFunc = color ? getYellowColor : getColor;
 
-  for (var i=0; i<ns.length; i++) {
-    ns[i].color=colorFunc(ns[i].level);
+  for (let i = 0; i < ns.length; i += 1) {
+    ns[i].color = colorFunc(ns[i].level);
     // Prevent snapping
     delete ns[i].x;
     delete ns[i].y;
@@ -111,8 +97,8 @@ function colorNodes(ns,color) {
 }
 
 // Set the width of some edges.
-function edgesWidth(es,width) {
-  for (var i=0; i<es.length; i++) {
+function edgesWidth(es, width) {
+  for (let i = 0; i < es.length; i += 1) {
     es[i].width = width;
   }
   edges.update(es);
@@ -121,26 +107,27 @@ function edgesWidth(es,width) {
 
 // Get the id of the edge connecting two nodes a and b
 function getEdgeConnecting(a, b) {
-  var edge = edges.get({filter:function(edge) {
-    return edge.from === a && edge.to === b;
-  }})[0];
-  if (edge instanceof Object) {
-    return edge.id;
-  }
+  const edge = edges.get({
+    filter: e => e.from === a && e.to === b,
+  })[0];
+
+  return (edge instanceof Object ? edge : {}).id;
 }
 
 // Get the network's center of gravity
 function getCenter() {
-  var nodePositions = network.getPositions();
-  var keys = Object.keys(nodePositions);
+  const nodePositions = network.getPositions();
+  const keys = Object.keys(nodePositions);
+
   // Find the sum of all x and y values
-  var xsum = 0; ysum = 0;
-  for (var i=0; i<keys.length; i++) {
-    var pos = nodePositions[keys[i]];
+  let xsum = 0; let ysum = 0;
+
+  Object.values(nodePositions).forEach((pos) => {
     xsum += pos.x;
     ysum += pos.y;
-  }
-  return [xsum/keys.length, ysum/keys.length]; // Average is sum divided by length
+  });
+
+  return [xsum / keys.length, ysum / keys.length]; // Average is sum divided by length
 }
 
 // Get the position in which nodes should be spawned given the id of a parent node.
@@ -148,23 +135,22 @@ function getCenter() {
 // leading to less chaotic node openings in large networks.
 function getSpawnPosition(parentID) {
   // Get position of the node with specified id.
-  var pos = network.getPositions(parentID)[parentID];
-  var x = pos.x, y=pos.y;
-  var cog = getCenter();
+  const { x, y } = network.getPositions(parentID)[parentID];
+  const cog = getCenter();
   // Distances from center of gravity to parent node
-  var dx = cog[0]-x, dy = cog[1]-y;
+  const dx = cog[0] - x; const dy = cog[1] - y;
 
-  var relSpawnX, relSpawnY;
+  let relSpawnX; let relSpawnY;
 
   if (dx === 0) { // Node is directly above center of gravity or on it, so slope will fail.
     relSpawnX = 0;
-    relSpawnY = -sign(dy)*100;
+    relSpawnY = -sign(dy) * 100;
   } else {
     // Compute slope
-    var slope = dy/dx;
+    const slope = dy / dx;
     // Compute the new node position.
-    var dis = 200; // Distance from parent. This should be equal to network.options.physics.springLength;
-    relSpawnX = dis / Math.sqrt( Math.pow(slope,2)+1 );
+    const dis = 200; // Distance from parent (keep equal to network.options.physics.springLength)
+    relSpawnX = dis / Math.sqrt((slope ** 2) + 1);
     relSpawnY = relSpawnX * slope;
   }
   return [Math.round(relSpawnX + x), Math.round(relSpawnY + y)];
