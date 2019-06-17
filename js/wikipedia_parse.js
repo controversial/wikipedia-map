e573bf07501d8366e5af8ed19d8016a49507282f
Luke Taylor <luke@deentaylor.com>
2019-06-16 22:40:14 -0400

    I lied about the coordinates problem going away

--- js/wikipedia_parse.js
+++ js/wikipedia_parse.js
@@ -46,7 +46,11 @@ function getPageHtml(pageName) {
 Get a DOM object for the first body paragraph in page HTML.
 @param {HtmlElement} element - An HTML element as returned by `getPageHtml`
 */
-const getFirstParagraph = element => element.querySelector('p:not(.mw-empty-elt)');
+const getFirstParagraph = element =>
+  // First paragraph that isn't marked as "empty"...
+  Array.from(element.querySelectorAll('p:not(.mw-empty-elt)'))
+    // ...and isn't the "coordinates" container
+    .find(p => !p.querySelector('#coordinates'));
 
 /**
 Get the name of each Wikipedia article linked.
