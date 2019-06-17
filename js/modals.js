0c1e9f6d1a50cb556bba3f8b64edb1fa2d719164
Luke Taylor <luke@deentaylor.com>
2019-06-16 22:35:46 -0400

    Update modals.js style

--- js/modals.js
+++ js/modals.js
@@ -10,38 +10,34 @@
 */
 
 function Modal(element, clickToDismiss) {
-  this.clickToDismiss = (clickToDismiss === undefined ? true:clickToDismiss);  // Allow clicking to dismiss by default
+  // Allow clicking to dismiss by default
+  this.clickToDismiss = (clickToDismiss === undefined ? true : clickToDismiss);
 
   // Construct a centered floating box
-  this.elem = document.createElement("div");
-  if (typeof element == "string") {
+  this.elem = document.createElement('div');
+  if (typeof element === 'string') {
     this.elem.innerHTML = element;
   } else {
     this.elem.appendChild(element);
   }
-  this.elem.className = "centered";
+  this.elem.className = 'centered';
 
-  this.backdrop = document.createElement("div");
-  this.backdrop.className = "modal-background transparent-blur";
+  this.backdrop = document.createElement('div');
+  this.backdrop.className = 'modal-background transparent-blur';
 
   this.backdrop.appendChild(this.elem);
 
   // Allow dismissing the modal with a click on the background
   this.backdrop.parent = this;
-  this.backdrop.onclick = function(event) {
-    if ((event.target.className.indexOf("modal-background") !== -1) &&
-        this.parent.clickToDismiss) { // Clicking on modal content won't hide it, only clicking the background will.
+  this.backdrop.addEventListener('click', (event) => {
+    // Clicking on modal content won't hide it, only clicking the background will.
+    if ((event.target.className.indexOf('modal-background') !== -1) && this.parent.clickToDismiss) {
       this.parent.close();
     }
-  };
+  });
 
   // Expose API
 
-  this.present = function() {
-    document.body.appendChild(this.backdrop);
-  };
-
-  this.close = function () {
-    document.body.removeChild(this.backdrop);
-  };
+  this.present = () => { document.body.appendChild(this.backdrop); };
+  this.close = () => { document.body.removeChild(this.backdrop); };
 }
