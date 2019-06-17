a47d169319929e217e471fed3658c95c5385f76c
Luke Taylor <luke@deentaylor.com>
2019-06-16 22:33:41 -0400

    Update main_functions.js style

--- js/main_functions.js
+++ js/main_functions.js
@@ -1,110 +1,126 @@
+/* global nodes, edges, isReset, startpages, getSpawnPosition, getNeutralId, wordwrap, unwrap, getColor, getEdgeColor, getEdgeConnecting, getSubPages, colorNodes, edgesWidth */ // eslint-disable-line max-len
 // This script contains the big functions that implement a lot of the core
 // functionality, like expanding nodes, and getting the nodes for a traceback.
 
 
 // -- GLOBAL VARIABLES -- //
-var isReset = true;
-var selectedNode = null;
-var traceedges = [];
-var tracenodes = [];
+window.isReset = true;
+window.selectedNode = null;
+window.traceedges = [];
+window.tracenodes = [];
 // ---------------------- //
 
 
-// AJAX callback to add to a node once data is recieved
-function expandNodeCallback(page,data) {
-  var node = nodes.get(page); //The node that was clicked
-  var level = node.level + 1; //Level for new nodes is one more than parent
-  var subpages = data; //Data returned from AJAX call
+// Callback to add to a node once data is recieved
+function expandNodeCallback(page, data) {
+  const node = nodes.get(page); // The node that was clicked
+  const level = node.level + 1; // Level for new nodes is one more than parent
+  const subpages = data;
 
   // Add all children to network
-  var subnodes = [];
-  var newedges = [];
+  const subnodes = [];
+  const newedges = [];
   // Where new nodes should be spawned
-  var nodeSpawn = getSpawnPosition(page);
-  //Create node objects
-  for (var i=0; i<subpages.length; i++) {
-    var subpage = subpages[i];
-    var subpageID = getNeutralId(subpage);
-    if (nodes.getIds().indexOf(subpageID) == -1) { //Don't add if node exists
-        subnodes.push({id:subpageID, label:wordwrap(decodeURIComponent(subpage),15), value:1,
-                       level:level, color:getColor(level), parent:page,
-                       x:nodeSpawn[0], y:nodeSpawn[1]}); //Add node
+  const [x, y] = getSpawnPosition(page);
+  // Create node objects
+  for (let i = 0; i < subpages.length; i += 1) {
+    const subpage = subpages[i];
+    const subpageID = getNeutralId(subpage);
+    if (nodes.getIds().indexOf(subpageID) === -1) { // Don't add if node exists
+      subnodes.push({
+        id: subpageID,
+        label: wordwrap(decodeURIComponent(subpage), 15),
+        value: 1,
+        level,
+        color: getColor(level),
+        parent: page,
+        x,
+        y,
+      });
     }
 
-    if (!getEdgeConnecting(page, subpageID)) { //Don't create duplicate edges in same direction
-      newedges.push({from: page, to: subpageID, color:getEdgeColor(level),
-                     level: level, selectionWidth:2, hoverWidth:0});
+    if (!getEdgeConnecting(page, subpageID)) { // Don't create duplicate edges in same direction
+      newedges.push({
+        from: page,
+        to: subpageID,
+        color: getEdgeColor(level),
+        level,
+        selectionWidth: 2,
+        hoverWidth: 0,
+      });
     }
   }
-  //Add the stuff to the nodes array
+
+  // Add the stuff to the nodes array
   nodes.add(subnodes);
   edges.add(newedges);
-
 }
-//Expand a node without freezing other stuff
+
+// Expand a node without freezing other stuff
 function expandNode(page) {
-  var label = nodes.get(page).label;
-  var pagename = unwrap(label);
+  const label = nodes.get(page).label;
+  const pagename = unwrap(label);
   getSubPages(pagename).then(data => expandNodeCallback(page, data));
 }
 
-//Get all the nodes tracing back to the start node.
+// Get all the nodes tracing back to the start node.
 function getTraceBackNodes(node) {
-  var finished = false;
-  var path = [];
-  while (! finished) { //Add parents of nodes until we reach the start
-    path.push(node);
-    if (startpages.indexOf(node) !== -1) { //Check if we've reached the end
+  let currentNode = node;
+  let finished = false;
+  const path = [];
+  while (!finished) { // Add parents of nodes until we reach the start
+    path.push(currentNode);
+    if (startpages.indexOf(currentNode) !== -1) { // Check if we've reached the end
       finished = true;
     }
-    node = nodes.get(node).parent; //Keep exploring with the node above.
+    currentNode = nodes.get(currentNode).parent; // Keep exploring with the node above.
   }
   return path;
 }
 
-//Get all the edges tracing back to the start node.
+// Get all the edges tracing back to the start node.
 function getTraceBackEdges(tbnodes) {
   tbnodes.reverse();
-  var path = [];
-  for (var i=0; i<tbnodes.length-1; i++) { //Don't iterate through the last node
-    path.push( getEdgeConnecting(tbnodes[i], tbnodes[i+1]) );
+  const path = [];
+  for (let i = 0; i < tbnodes.length - 1; i += 1) { // Don't iterate through the last node
+    path.push(getEdgeConnecting(tbnodes[i], tbnodes[i + 1]));
   }
   return path;
 }
 
-//Reset the color of all nodes, and width of all edges.
+// Reset the color of all nodes, and width of all edges.
 function resetProperties() {
   if (!isReset) {
-    selectedNode = null;
-    //Reset node color
-    var modnodes = tracenodes.map(function(i){return nodes.get(i);});
+    window.selectedNode = null;
+    // Reset node color
+    const modnodes = window.tracenodes.map(i => nodes.get(i));
     colorNodes(modnodes, 0);
-    //Reset edge width and color
-    var modedges = traceedges.map(function(i){
-      var e=edges.get(i);
-      e.color=getEdgeColor(nodes.get(e.to).level);
+    // Reset edge width and color
+    const modedges = window.traceedges.map((i) => {
+      const e = edges.get(i);
+      e.color = getEdgeColor(nodes.get(e.to).level);
       return e;
     });
     edgesWidth(modedges, 1);
-    tracenodes = [];
-    traceedges = [];
+    window.tracenodes = [];
+    window.traceedges = [];
   }
 }
 
-//Highlight the path from a given node back to the central node.
+// Highlight the path from a given node back to the central node.
 function traceBack(node) {
-  if (node != selectedNode) {
-    selectedNode = node;
+  if (node !== window.selectedNode) {
+    window.selectedNode = node;
     resetProperties();
-    tracenodes = getTraceBackNodes(node);
-    traceedges = getTraceBackEdges(tracenodes);
-    //Color nodes yellow
-    var modnodes = tracenodes.map(function(i){return nodes.get(i);});
+    window.tracenodes = getTraceBackNodes(node);
+    window.traceedges = getTraceBackEdges(window.tracenodes);
+    // Color nodes yellow
+    const modnodes = window.tracenodes.map(i => nodes.get(i));
     colorNodes(modnodes, 1);
-    //Widen edges
-    var modedges = traceedges.map(function(i){
-      var e=edges.get(i);
-      e.color={inherit:"to"};
+    // Widen edges
+    const modedges = window.traceedges.map((i) => {
+      const e = edges.get(i);
+      e.color = { inherit: 'to' };
       return e;
     });
     edgesWidth(modedges, 5);
