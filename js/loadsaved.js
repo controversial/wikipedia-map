7897c19c6b1d8fc0d13798bb3afab6c7d3ed7bba
Luke Taylor <luke@deentaylor.com>
2019-06-16 22:33:30 -0400

    Update loadsaved.js style

--- js/loadsaved.js
+++ js/loadsaved.js
@@ -1,20 +1,20 @@
+/* global network, makeNetwork, loadGraph, Progress, Modal */
 // Load a saved graph if an ID is provided in the query string
 
 function loadSaved() {
   if (window.location.search) {
-    progressbar = new Progress("Restoring saved graph...");
-    modalWindow = new Modal(progressbar.container, false);
+    window.progressbar = new Progress('Restoring saved graph...');
+    const modalWindow = new Modal(window.progressbar.container, false);
     modalWindow.present();
     // Make the blank network
     makeNetwork();
-    progressbar.progress(0.02);
+    window.progressbar.progress(0.02);
     // Set up event listeners for the loading (starting at 2%)
-    network.on("stabilizationProgress", function(params) {
-      progressbar.progress(params.iterations / params.total + 0.02);
+    network.on('stabilizationProgress', (params) => {
+      window.progressbar.progress((params.iterations / params.total) + 0.02);
     });
-    network.once("stabilizationIterationsDone", function() {
-      modalWindow.close();
-    });
+
+    network.once('stabilizationIterationsDone', () => { modalWindow.close(); });
     loadGraph(window.location.search.substring(1));
   }
 }
