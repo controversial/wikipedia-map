393de5db240923f26c9c3c48174c80049a016e72
Luke Taylor <luke@deentaylor.com>
2020-12-28 14:22:11 -0500

    Rewrite tour content

--- js/help.js
+++ js/help.js
@@ -16,64 +16,10 @@ const shepherd = new Shepherd.Tour({
 // Add steps to the Shepherd tour.
 
 shepherd.addStep({
-  text: 'Wikipedia Map is an interactive web app for visualizing the ' +
-        'connections between Wikipedia pages.',
-  buttons: [
-    {
-      text: 'Skip',
-      classes: 'shepherd-button-secondary',
-      action: shepherd.cancel,
-    },
-    {
-      text: 'Next',
-      classes: 'shepbtn',
-      action() {
-        shepherd.next();
-        buttons.style.opacity = 1;
-      },
-    },
-  ],
-});
-
-shepherd.addStep({
-  text: 'Wikipedia Map is 100% open source, as are each of the libraries on ' +
-        'which it depends. You can visit Wikipedia Map on GitHub.',
-  attachTo: '#github left',
-  buttons: [
-    {
-      text: 'Back',
-      classes: 'shepherd-button-secondary',
-      action: shepherd.back,
-    },
-    { // Add a button to visit on GitHub
-      text: 'Visit',
-      classes: 'shepherd-button-secondary',
-      action() {
-        window.open('https://github.com/The-Penultimate-Defenestrator/wikipedia-map');
-      },
-    },
-    {
-      text: 'Next',
-      classes: 'shepbtn',
-      action() {
-        shepherd.next();
-        buttons.style.opacity = 0.3;
-        formbox.style.opacity = 1;
-      },
-    },
-  ],
-  tetherOptions: {
-    attachment: 'bottom right',
-    targetAttachment: 'left center',
-    offset: '-55px 20px',
-  },
-});
-
-shepherd.addStep({
   text: [
     'Input the name of a Wikipedia article here.',
-    'You can compare multiple topics by pressing <code>,</code> ' +
-    '<code>Tab</code> or <code>Enter</code> after each one.',
+    'You can compare multiple topics by pressing <kbd>,</kbd> ' +
+    '<kbd>Tab</kbd> or <kbd>Enter</kbd> after each one.',
   ],
   attachTo: '#input bottom',
   buttons: [
@@ -93,9 +39,52 @@ shepherd.addStep({
 shepherd.addStep({
   text: [
     "Once you're done, submit your query.",
-    'Wikipedia Map will create a node for each input.',
+    'Wikipedia Map will create a node for each input topic.',
   ],
   attachTo: '#submit bottom',
+  buttons: [
+    {
+      text: 'Back',
+      classes: 'shepherd-button-secondary',
+      action: shepherd.back,
+    },
+    {
+      text: 'Next',
+      classes: 'shepbtn',
+      action: shepherd.next,
+    },
+  ],
+  tetherOptions: {
+    attachment: 'top left',
+    targetAttachment: 'bottom center',
+    offset: '0px -35px',
+  },
+});
+
+shepherd.addStep({
+  text: [
+    'Click a node to expand it.',
+    'Expanding a node creates a new node for each Wikipedia article linked in the first paragraph of the article whose node you clicked.',
+    '<img src="https://images.prismic.io/luke/db049805-b070-43c5-a412-d44c5ac3a4d7_wikipedia-expand.gif" alt="Expanding a Wikipedia Map node" style="width: 410px; height: 410px;" />',
+  ],
+  buttons: [
+    {
+      text: 'Back',
+      classes: 'shepherd-button-secondary',
+      action: shepherd.back,
+    },
+    {
+      text: 'Next',
+      classes: 'shepbtn',
+      action: shepherd.next,
+    },
+  ],
+});
+
+shepherd.addStep({
+  text: [
+    'Keep expanding nodes to build a map and connect topics!',
+  ],
   buttons: [
     {
       text: 'Back',
@@ -108,11 +97,6 @@ shepherd.addStep({
       action: shepherd.next,
     },
   ],
-  tetherOptions: {
-    attachment: 'top left',
-    targetAttachment: 'bottom center',
-    offset: '0px -35px',
-  },
 });
 
 // Take away the info box when the tour has started...
